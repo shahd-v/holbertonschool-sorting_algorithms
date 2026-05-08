4fa5490b0db9049507146e4f2e346eadb2f9a08f
ahaad14550-boop <ahaad.14450@gmail.com>
2026-05-08 14:36:43 +0000

    Fix listint_t type errors in sort.h and implement insertion sort

--- sort.h
+++ sort.h
@@ -6,5 +6,7 @@
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void insertion_sort_list(listint_t **list);
 
 #endif
