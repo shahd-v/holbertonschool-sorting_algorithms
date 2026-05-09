16211b3acc65218f54796c96a25338f47d2c003b
shahd-v <shahdfaisalr@gmail.com>
2026-05-09 17:38:32 +0300

    add prototype of selection_sort and quick_sort functions

--- sort.h
+++ sort.h
@@ -27,4 +27,7 @@ void insertion_sort_list(listint_t **list);
 
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif
